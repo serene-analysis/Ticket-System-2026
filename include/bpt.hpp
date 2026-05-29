a52984829d780dc809d506d9b1962f333f742650
serene_analysis <2742074247@qq.com>
2026-05-29 19:36:29 +0800

    final : turn down cache size

--- include/bpt.hpp
+++ include/bpt.hpp
@@ -15,7 +15,7 @@ using std::ios;
 //#define merge_DEBUG
 //#define overall_DEBUG
 
-template<typename Key, typename Value, int MAX_CAP = 128>
+template<typename Key, typename Value, int MAX_CAP = 64>
 class LRUCache {
 private:
     static const int HASH_SIZE = 1024;
